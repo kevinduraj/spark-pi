cf3e8c1eab8555e261b2c87466d5e331a972de7e
Kevin Duraj <kevin.x.duraj.-nd@disney.com>
2017-08-14 15:23:16 -0700

    2017-08-14 15:23

---
+++ open.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+find . -name "*.scala" -exec vim {} +
+
--- run.sh
+++ run.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 #-----------------------------------------------------------------------------------#
   if [ "$1" == "0" ] 2>/dev/null; then
-    echo "sbt clean && sbt package"
-    sbt clean && sbt package
+    echo "sbt clean package"
+    sbt clean package
 
 #-----------------------------------------------------------------------------------#
 elif [ "$1" == "1" ] 2>/dev/null; then
 
 time spark-submit                                       \
     --class "org.apache.spark.examples.SparkPi"         \
-    --master spark://69.13.39.34:7077                   \
-    --driver-memory   32G                               \
-    --executor-memory 16G                               \
+    --master "local[4]"                                 \
+    --driver-memory   16G                               \
+    --executor-memory 8G                                \
     target/scala-2.11/spark-pi_2.11-0.1.jar             \
-    "1000" 
+    "100" 
+
+#-----------------------------------------------------------------------------------#
+elif [ "$1" == "2" ] 2>/dev/null; then
+
+time spark-submit                                       \
+    --class "org.apache.spark.examples.SparkPi"         \
+    --master spark://69.13.39.34:7077                   \
+    --driver-memory   16G                               \
+    --executor-memory 8G                                \
+    target/scala-2.11/spark-pi_2.11-0.1.jar             \
+    "100" 
 
 #-----------------------------------------------------------------------------------#
 else
@@ -21,8 +32,8 @@ else
   echo "+---------------------------------------------+"
   echo "| ./run.sh 0 -- sbt clean && sbt package      |"
   echo "+---------------------------------------------+"
-  echo "| ./run.sh 1 -- SparkPi                       |"
-  echo "| ./run.sh 4 -- export_null                   |"
+  echo "| ./run.sh 1 -- SparkPi Local                 |"
+  echo "| ./run.sh 2 -- SparkPi Remote                |"
   echo "+---------------------------------------------+"
 
 fi
